a8d55d9d31dd5d1a57602ac2dc29c06e9b095e27
morwen80 <ribesnero80@gmail.com>
2019-11-28 18:01:50 +0100

    removed keywords for retweets - floral and floralphotography

--- twitterBot.js
+++ twitterBot.js
@@ -10,49 +10,49 @@ const config = {
 const Twitter = new twit(config);
 
 let retweet = function() {
-    let params = {
-        q: '#flower OR #flowers OR #florist OR #floralphotography OR #floral',
-        result_type: 'mixed',
-        lang: 'en',
-        filter: 'safe'
-    }
-
-    Twitter.get('search/tweets', params, function(err, data) {
-          // if there is no error
-          if (!err) {
-             // loop through the first 4 returned tweets
-            for (let i = 0; i < 4; i++) {
-              // iterate through those first four defining a rtId that is equal to the value of each of those tweets' ids
-            let rtId = data.statuses[i].id_str;
-              // the post action
-
-            Twitter.post('statuses/retweet/:id', {id: rtId}, function(err, response) {
-              if (response) {
-                console.log('Successfully retweeted');
-              }
-              if (err) {
-                console.log(err);
-              }
-              else {
-                  // catch all log if the search could not be executed
-                console.log('Could not search tweets.');
-              }
-            });
-
-            Twitter.post('favorites/create', {id: rtId}, function(err, response){
-              // if there was an error while 'favorite'
-              if(err){
-                console.log('CANNOT BE FAVORITED');
-              }
-              else{
-                console.log('FAVORITED!');
-              }
-            });
-          }
-        }
-
-      });
+  let params = {
+    q: '#flower OR #flowers OR #florist',
+    result_type: 'mixed',
+    lang: 'en',
+    filter: 'safe'
   }
+
+  Twitter.get('search/tweets', params, function(err, data) {
+    // if there is no error
+    if (!err) {
+       // loop through the first 4 returned tweets
+      for (let i = 0; i < 4; i++) {
+        // iterate through those first four defining a rtId that is equal to the value of each of those tweets' ids
+      let rtId = data.statuses[i].id_str;
+        // the post action
+
+  Twitter.post('statuses/retweet/:id', {id: rtId}, function(err, response) {
+    if (response) {
+      console.log('Successfully retweeted');
+    }
+    if (err) {
+      console.log(err);
+    }
+    else {
+        // catch all log if the search could not be executed
+      console.log('Could not search tweets.');
+    }
+  });
+
+    Twitter.post('favorites/create', {id: rtId}, function(err, response){
+      // if there was an error while 'favorite'
+      if(err){
+        console.log('CANNOT BE FAVORITED');
+      }
+      else {
+        console.log('FAVORITED!');
+      }
+    });
+  }
+}
+
+  });
+}
 
 
 let followBack = function(){
@@ -63,8 +63,8 @@ let followBack = function(){
     console.log(err)
   } else {
     console.log(data)
-  }
-});
+    }
+  });
 }
 
 retweet();
